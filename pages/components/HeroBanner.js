d4eaf8853f2fd0851a6b753ce45f772fd248547d
Elysia Gabe <elysiagabe@gmail.com>
2020-06-18 11:41:38 -0700

    Adds links to hero with arrow icon animations

--- pages/components/HeroBanner.js
+++ pages/components/HeroBanner.js
@@ -6,6 +6,14 @@ const HeroBanner = () => {
                 <div className="hero-blurb">
                     <h1>Aloha, I'm Elysia</h1>
                     <p>A full-stack web developer based in Oakland with roots in Hawaiʻi. I’m passionate about building impactful, scalable and accessible tech solutions for social good. </p>
+                    <div className="hero-button">
+                        <img src="/green_arrow.png" id="icon" />
+                        <a href="#">Get to know me</a>
+                    </div>
+                    <div className="hero-button">
+                        <img src="/green_arrow.png" id="icon" />
+                        <a href="#">Check out my work</a>
+                    </div>
                 </div>
             </section>
             <img src="/bottom_wave.png" className="bottom-wave" />
@@ -42,11 +50,52 @@ const HeroBanner = () => {
                     margin: 0;
                     padding: 0;
                 }
-        
+
+                .hero-button {
+                    display: flex;
+                    align-items: center;
+                    margin-bottom: 12px;
+                }
+
+                .hero-button img {
+                    width: 28px;
+                    margin-right: 12px;
+                }
+
+                .hero-button a {
+                    text-decoration: none;
+                    color: #00857D;
+                    text-transform: uppercase;
+                    font-size: 1.4rem;
+                    font-weight: 500;
+                }
+
                 .bottom-wave {
                     position: absolute;
                     width: 100%;
                     top: 70vh;
+                }
+
+                .hero-button:hover > #icon {
+                    animation: swing 1s ease;
+                    animation-iteration-count: 1;
+                }
+
+                @keyframes swing {
+                    20% {
+                    transform: translateX(3px);
+                    }
+                    40% {
+                    transform: translateX(-3px);
+                    }
+                    60% {
+                    transform: translateX(2px);
+                    }
+                    80% {
+                    transform: translateX(-1px);
+                    }
+                    100% {
+                    transform: translateX(0);
                 }
             `}</style>
         </section>
